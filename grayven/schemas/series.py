536e62504cb5a7304ba65c3fcdea5786b66acc3b
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2024-12-23 15:14:43 +1300

    Add more id properties (#5)
     - Add publisher_id property to Series
     - Add series_id property to BasicIssue
     - Add more docs

--- grayven/schemas/series.py
+++ grayven/schemas/series.py
@@ -19,21 +19,21 @@ class Series(BaseModel):
     """Contains fields for all Series.
 
     Attributes:
-      active_issues:
-      api_url:
-      binding:
-      color:
-      country:
-      dimensions:
+      active_issues: A list of URLs for active issues in the series.
+      api_url: Url to the resource in the GCD API.
+      binding: The binding type of the series.
+      color: The color information of the series.
+      country: The country where the series is published.
+      dimensions: The dimensions of the series.
       issue_descriptors:
-      language:
-      name:
-      notes:
+      language: The language of the series.
+      name: The name of the series.
+      notes: Additional notes about the series.
       paper_stock:
-      publisher:
-      publishing_format:
-      year_began:
-      year_ended:
+      publisher: Url to the Publisher of this resource in the GCD API.
+      publishing_format: The publishing format of the series.
+      year_began: The year the series began.
+      year_ended: The year the series ended.
     """
 
     active_issues: list[HttpUrl]
@@ -54,8 +54,14 @@ class Series(BaseModel):
 
     @property
     def id(self) -> int:
-        """The Series id, extracted from the `api_url`."""
-        match = re.search(r"/series/(\d+)/", str(self.api_url))
-        if match:
+        """The Series id, extracted from the `api_url` field."""
+        if match := re.search(r"/series/(\d+)/", str(self.api_url)):
+            return int(match.group(1))
+        raise ValueError("Unable to get id from url: '%s'", self.api_url)
+
+    @property
+    def publisher_id(self) -> int:
+        """The Publisher id, extracted from the `publisher` field."""
+        if match := re.search(r"/publisher/(\d+)/", str(self.api_url)):
             return int(match.group(1))
         raise ValueError("Unable to get id from url: '%s'", self.api_url)
